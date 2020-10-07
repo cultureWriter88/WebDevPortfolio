e1fc28b1f85b932f019b58863534a89aefbf95a9
Thomas Perez <culturewriter88@gmail.com>
2020-10-07 00:06:01 -0400

    Continuing work

--- COLOR PALETTES/script.js
+++ COLOR PALETTES/script.js
@@ -19,3 +19,47 @@ function show() {
     } 
 }; 
 
+
+// SLIDESHOW
+// REBUILD IN ES6
+var slideIndex = 1;
+var t;
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  slideIndex = slideIndex + n;
+  clearTimeout(t);
+  showSlides(slideIndex);
+  console.log(slideIndex);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+
+  if (n == undefined) {
+    n = ++slideIndex;
+  }
+
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  t = setTimeout(showSlides, 5000);
+}
+// SLIDESHOW ENDS
