f10e3ffa40cba05619fbc03bb13a7d37e5f4c5b9
cultureWriter <culturewriter88@gmail.com>
2020-11-19 19:09:43 -0500

    Update main.js

--- MDN|image-gallery/main.js
+++ MDN|image-gallery/main.js
@@ -7,10 +7,12 @@ const overlay = document.querySelector('.overlay');
 /* Looping through images */
 for (let i = 1; i < 6;i++){
   const newImage = document.createElement('img');
-    newImage.setAttribute("src", "images/pic" + i + ".jpg");
-    thumbBar.appendChild(newImage);
-    //Here I am working on making main image on
-    thumbBar.onclick = document.getElementsByClassName("thumb-bar").src = "images/pic" + i + ".jpg";
-    console.log(thumbBar);
+  newImage.setAttribute("src", "images/pic" + i + ".jpg");
+  thumbBar.appendChild(newImage);
+
+  //Here I am working on making main image on 
+  newImage.onclick = overlay.src =  "images/pic + i + .jpg";
+  console.log(newImage);
+
 }
 /* Wiring up the Darken/Lighten button */
