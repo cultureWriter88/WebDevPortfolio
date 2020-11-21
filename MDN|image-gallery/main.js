15baec8eef118601161491bbae347da9cfc165c4
Thomas Perez <culturewriter88@gmail.com>
2020-11-21 10:03:56 -0500

    Update main.js

--- MDN|image-gallery/main.js
+++ MDN|image-gallery/main.js
@@ -5,14 +5,17 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
-for (let i = 1; i < 6;i++){
+for (let i = 1; i < 6; i++){
   const newImage = document.createElement('img');
   newImage.setAttribute("src", "images/pic" + i + ".jpg");
   thumbBar.appendChild(newImage);
 
   //Here I am working on making main image on 
-  newImage.onclick = overlay.src =  "images/pic3.jpg";
-  console.log(newImage);
-
+  newImage.onclick = function(e){
+    displayedImage.src = e.target.src;
+  }
 }
 /* Wiring up the Darken/Lighten button */
+btn.onclick = function(){
+  btn.getAttribute("dark", "light");
+}
