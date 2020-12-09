5702896c85651e89d74a79b43ae4486448fc5e8c
Thomas Perez <culturewriter88@gmail.com>
2020-12-09 17:50:39 -0500

    Fixed if else if

--- State Facts Sheet/script.js
+++ State Facts Sheet/script.js
@@ -4,20 +4,37 @@ let inputState = document.getElementById("input").value;
 submitButton.addEventListener("click", stateInfo);
 
 function stateInfo(){
-    document.querySelector("html").style.backgroundColor = "violet";
-    if (inputState = "New York"){
-      'use strict';
+  let inputState = document.getElementById("input").value;
+    if (inputState === "New Jersey"){
+      document.querySelector("html").style.backgroundColor = "yellow";
+      document.getElementById("name").innerHTML = "New Jersey";
+      document.getElementById("admitted").innerHTML = "December 18, 1787";
+      document.getElementById("capital").innerHTML = "Trenton";
+      document.getElementById("total-area").innerHTML = "8,722.58 square miles";
+      document.getElementById("state-abbreviation").innerHTML = "NJ";
+      document.getElementById("name-origin").innerHTML = "English Channel island Jersey";
+    } else if (inputState === "New Mexico") {
+      document.querySelector("html").style.backgroundColor = "blue";
+      document.getElementById("name").innerHTML = "New Mexico";
+      document.getElementById("admitted").innerHTML = "January 6, 1912";
+      document.getElementById("capital").innerHTML = "Sante Fe";
+      document.getElementById("total-area").innerHTML = "121,590 square miles";
+      document.getElementById("state-abbreviation").innerHTML = "NM";
+      document.getElementById("name-origin").innerHTML = "Aztec Valley of Mexico (not the country)";
+    } else if(inputState === "New York"){
+      document.querySelector("html").style.backgroundColor = "red";
       document.getElementById("name").innerHTML = "New York";
       document.getElementById("admitted").innerHTML = "July 26, 1788";
       document.getElementById("capital").innerHTML = "Albany";
       document.getElementById("total-area").innerHTML = "54,556 square miles";
       document.getElementById("state-abbreviation").innerHTML = "NY";
       document.getElementById("name-origin").innerHTML = "17th Century Duke of York";
-    } else if (inputState = "New Jersey"){
-      document.querySelector("html").style.backgroundColor = "red";
-      alert("Try Again!")
+    } else {
+      document.querySelector("html").style.backgroundColor = "green";
     }
-};
+
+}
+
 
 
       // } else if (inputState = "New Jersey"){
